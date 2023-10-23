2be3ffcab3ef60c888f81f859c709897b82c1000
vsvang87 <vsvang@gmail.com>
2023-10-23 13:42:04 -0400

    add login, sign up, post, get

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 const express = require("express");
-const path = require("path");
 const ejs = require("ejs");
-const app = new express();
+const app = express();
 
-//public folder
+//middle ware
 app.use(express.static("public"));
+app.use(express.json());
 app.set("view engine", "ejs");
+
+//json web token
+// const jwt = require("jsonwebtoken");
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
-
+//home page
 app.get("/", (req, res) => {
   res.render("index.html");
 });
-app.get("/about", (req, res) => {
-  res.render("about.html");
+//signup
+app.get("/signup", (req, res) => {
+  res.render("signup.html");
+});
+app.post("/signup", (req, res) => {
+  res.send("new sign up user");
+});
+//log in route
+app.get("/login", (req, res) => {
+  res.render("user_profile.html");
+});
+app.post("/login", (req, res) => {
+  const { username, email, password } = req.body;
+  res.send("user login");
 });
