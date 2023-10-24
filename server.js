45ba0afb6f76ef95f94c86f0b128931b24de8806
vsvang87 <vsvang@gmail.com>
2023-10-24 13:04:29 -0400

    add token, auth login

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require("express");
-const ejs = require("ejs");
+// const ejs = require("ejs");
 const app = express();
 
+//json web token
+const jwt = require("jsonwebtoken");
 //middle ware
 app.use(express.static("public"));
 app.use(express.json());
 app.set("view engine", "ejs");
-
-//json web token
-// const jwt = require("jsonwebtoken");
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
@@ -17,18 +16,72 @@ app.listen(3000, () => {
 app.get("/", (req, res) => {
   res.render("index.html");
 });
+app.post("/", verifyToken, (req, res) => {
+  jwt.verify(req.token, "secretkey", (err, auth) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      res.json({
+        message: "Posted Created",
+        auth,
+      });
+    }
+  });
+  res.json({
+    message: "Posted created",
+  });
+  console.log("Post Created");
+});
 //signup
 app.get("/signup", (req, res) => {
   res.render("signup.html");
 });
 app.post("/signup", (req, res) => {
-  res.send("new sign up user");
+  // const { username, email, password } = req.body;
+  // console.log(username, email, password);
+  // res.send("new sign up user");
 });
+//FORMAT TOKEN
+//Authorization: valueHead <access_token>
+
+//verify token
+function verifyToken(req, res, next) {
+  //Get auth header value
+  const valueHeader = req.headers["authorization"];
+
+  //check if valueHeader is undefined
+  if (typeof valueHeader !== "undefined") {
+    const bearer = valueHeader.split(" ");
+    //Get token from array
+    const bearerToken = bearer[1];
+    //Set token
+    req.token = bearerToken;
+    //Next
+    next();
+  } else {
+    res.sendStatus(403);
+  }
+}
 //log in route
 app.get("/login", (req, res) => {
   res.render("user_profile.html");
 });
 app.post("/login", (req, res) => {
-  const { username, email, password } = req.body;
-  res.send("user login");
+  const user = {
+    id: 1,
+    username: "webmaster00",
+    email: "webmaster@test.com",
+    password: "testing",
+  };
+
+  jwt.sign({ user }, "secretkey", (err, token) => {
+    res.json({
+      token,
+    });
+    console.log(token);
+  });
+
+  // const { username, password } = req.body;
+  // console.log(username, password);
+  // res.send("user login");
 });
