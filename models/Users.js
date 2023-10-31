22af9223433e4a049c9751f72d4c7f104219eba3
vsvang87 <vsvang@gmail.com>
2023-10-31 12:40:01 -0400

    add ejs, schema, routes, controllers

---
+++ models/Users.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { isEmail } = require("validator");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: [true, "please enter an email"],
+    unique: true,
+    lowercase: true,
+    validate: [isEmail, "please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "please enter a password"],
+    minlength: [6, "needs minimum length of 6 characters"],
+  },
+});
+
+//trigger a function after doc is saved to database
+userSchema.post("save", function (doc, next) {
+  console.log("new user was created & saved!", doc);
+});
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -1,123 +1,120 @@
 const express = require("express");
-const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const authRoutes = require("./routes/authRoutes");
 
-const jwt = require("jsonwebtoken"); //JSON Web Token
+const app = new express();
+// const ejs = require("ejs");
+app.set("view engine", "ejs");
+
+//JSON Web Token
+const jwt = require("jsonwebtoken");
 //middle ware
 app.use(express.static("public"));
 app.use(express.json());
-// app.set("view engine", "ejs");
+
+//connect to Database
+mongoose.connect("mongodb://localhost:27017");
+
 //App listening on port 3000
 app.listen(3000, () => {
-  console.log("Server listening on port 3000");
+  console.log("Server running on port 3000");
 });
-//home page
+
 app.get("/", (req, res) => {
-  res.render("index.html");
+  res.render("index");
 });
-app.post("/", verifyToken, (req, res) => {
-  jwt.verify(req.token, "secretkey", (err, auth) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      res.json({
-        message: "Posted Created",
-        auth,
-      });
-    }
-  });
-  res.json({
-    message: "Posted created",
-  });
-  console.log("Post Created");
-});
-//signup routes
-app.get("/signup", (req, res) => {
-  res.json(users);
-});
-//POST routes
-const users = []; //Storing users //This will go inside of database
-app.post("/signup", async (req, res) => {
-  try {
-    const salt = await bcrypt.genSalt();
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-    const user = {
-      id: Date.now().toString(),
-      username: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    };
-    users.push(user);
-    console.log(users);
-    res.redirect("/login.html");
-  } catch (e) {
-    console.log(e);
-    res.status(500).send();
-  }
-});
-//PUT routes
-app.put("/signup", (req, res) => {
-  console.log(req.body);
-  res.json({
-    message: "update successful",
-  });
-});
-//verify token
-function verifyToken(req, res, next) {
-  //Get auth header value
-  const valueHeader = req.headers["authorization"];
+app.use(authRoutes);
 
-  //check if valueHeader is undefined
-  if (typeof valueHeader !== "undefined") {
-    const bearer = valueHeader.split(" ");
-    //Get token from array
-    const bearerToken = bearer[1];
-    //Set token
-    req.token = bearerToken;
-    //Next
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-}
+// //home page
+// app.get("/", (req, res) => {
+//   res.render("index");
+// });
+// app.post("/", verifyToken, (req, res) => {
+//   jwt.verify(req.token, "secretkey", (err, auth) => {
+//     if (err) {
+//       res.sendStatus(403);
+//     } else {
+//       res.json({
+//         message: "Posted Created",
+//         auth,
+//       });
+//     }
+//   });
+//   res.json({
+//     message: "Posted created",
+//   });
+//   console.log("Post Created");
+// });
+// //signup routes
+// app.get("/signup", (req, res) => {
+//   res.json(users);
+// });
+// //POST routes
+// const users = []; //Storing users //This will go inside of database
+// app.post("/signup", async (req, res) => {
+//   try {
+//     const salt = await bcrypt.genSalt();
+//     const hashedPassword = await bcrypt.hash(req.body.password, salt);
+//     const user = {
+//       id: Date.now().toString(),
+//       username: req.body.username,
+//       email: req.body.email,
+//       password: hashedPassword,
+//     };
+//     users.push(user);
+//     console.log(users);
+//     res.redirect("/login.html");
+//   } catch (e) {
+//     console.log(e);
+//     res.status(500).send();
+//   }
+// });
+// //PUT routes
+// app.put("/signup", (req, res) => {
+//   console.log(req.body);
+//   res.json({
+//     message: "update successful",
+//   });
+// });
+// //verify token
+// function verifyToken(req, res, next) {
+//   //Get auth header value
+//   const valueHeader = req.headers["authorization"];
+//   //check if valueHeader is undefined
+//   if (typeof valueHeader !== "undefined") {
+//     const bearer = valueHeader.split(" ");
+//     //Get token from array
+//     const bearerToken = bearer[1];
+//     //Set token
+//     req.token = bearerToken;
+//     //Next
+//     next();
+//   } else {
+//     res.sendStatus(403);
+//   }
+// }
 
-//log in route
-app.get("/login", (req, res) => {
-  res.render("login.html");
-});
-app.post("/login", async (req, res, next) => {
-  // const user = {
-  //   id: 1,
-  //   username: "webmaster00",
-  //   email: "webmaster@test.com",
-  //   password: "testing",
-  // };
-  //Check if there is a user
-  const { username, password } = req.body;
-  if (!username || !password) {
-    return res.status(400).json({ message: "Username or Password not found" });
-  } else {
-    jwt.sign({ users }, "secretkey", (err, token) => {
-      res.json({
-        token,
-      });
-      console.log(token);
-    });
-    return res.redirect("/load.html");
-  }
-});
-
-//Log Out Route
-app.delete("/logout", (req, res) => {
-  if (req.session) {
-    req.session.destroy((err) => {
-      if (err) {
-        res.status(400).send("Unable to log out");
-      } else {
-        res.send("Logout successful");
-      }
-    });
-  } else {
-    res.end();
-  }
-});
+// //log in route
+// app.get("/login", (req, res) => {
+//   res.render("login.html");
+//   // url parameters
+//   // decoding the tokens
+//   //grab the username and password and look up in the database
+// });
+// app.post("/login", async (req, res, next) => {
+//   //Check if there is a user
+//   const { username, password } = req.body;
+//   if (!username || !password) {
+//     return res.status(400).json({ message: "Username or Password not found" });
+//   } else {
+//     jwt.sign({}, "secretkey", (err, token) => {
+//       res.json({
+//         token,
+//       });
+//       console.log(token);
+//     });
+//     return res.redirect("/load.html");
+//   }
+// });
